e0a981abe722a81be42e7cd9ee52e5431c056843
voomdoon <nilreb24-github20141105@yahoo.de>
2024-03-13 05:45:18 +0100

    feat: add TableStringGenerator for String table

---
+++ src/main/java/de/voomdoon/util/tostring/table/TableStringGenerator.java
@@ -0,0 +1,370 @@
+package de.voomdoon.util.tostring.table;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+//FEATURE null value
+//FEATURE date and time right alignment 
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+public class TableStringGenerator {
+
+	/**
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	private static class Context {
+
+		/**
+		 * @author André Schulz
+		 *
+		 * @since 0.1.0
+		 */
+		private static class ColumnContext {
+
+			/**
+			 * @author André Schulz
+			 *
+			 * @since 0.1.0
+			 */
+			private class Initializer {
+
+				/**
+				 * @param column
+				 * @return
+				 * @since 0.1.0
+				 */
+				private int getTextWidth(List<String> column) {
+					return column.stream().mapToInt(String::length).max().orElse(0);
+				}
+
+				/**
+				 * @param column
+				 * @since 0.1.0
+				 */
+				private void initNumberWidths(List<String> column) {
+					for (String cell : column) {
+						initNumberWidths(cell);
+					}
+				}
+
+				/**
+				 * @param cell
+				 * @since 0.1.0
+				 */
+				private void initNumberWidths(String cell) {
+					// TODO rework
+
+					if (INTEGER_PATTERN.matcher(cell).matches()) {
+						int left = cell.length();
+						numberWidthLeft = Math.max(numberWidthLeft, left);
+					} else if (REAL_PATTERN.matcher(cell).matches()) {
+						int index = cell.indexOf('.');
+						int left = index;
+						int right = cell.length() - index - 1;
+
+						numberWidthLeft = Math.max(numberWidthLeft, left);
+						numberWidthRight = Math.max(numberWidthRight, right);
+					}
+				}
+			}
+
+			/**
+			 * @since 0.1.0
+			 */
+			private static final Pattern INTEGER_PATTERN = Pattern.compile("\\d+");
+
+			/**
+			 * @since 0.1.0
+			 */
+			private static final Pattern REAL_PATTERN = Pattern.compile("(\\d?\\.\\d+|\\d+\\.\\d?)");
+
+			/**
+			 * @since 0.1.0
+			 */
+			private int numberWidthLeft;
+
+			/**
+			 * @since 0.1.0
+			 */
+			private int numberWidthRight;
+
+			/**
+			 * @since 0.1.0
+			 */
+			private int textWidth;
+
+			/**
+			 * DOCME add JavaDoc for constructor ColumnContext
+			 * 
+			 * @param column
+			 * @since 0.1.0
+			 */
+			public ColumnContext(List<String> column) {
+				Initializer initializer = new Initializer();
+				textWidth = initializer.getTextWidth(column);
+				initializer.initNumberWidths(column);
+			}
+
+			/**
+			 * @param cell
+			 * @return
+			 * @since 0.1.0
+			 */
+			public Padding getPadding(String cell) {
+				// TODO rework
+
+				if (INTEGER_PATTERN.matcher(cell).matches()) {
+					return getIntegerPadding(cell);
+				} else if (REAL_PATTERN.matcher(cell).matches()) {
+					return getRealPadding(cell);
+				}
+
+				return getTextPadding(cell);
+			}
+
+			/**
+			 * @param cell
+			 * @return
+			 * @since 0.1.0
+			 */
+			private Padding getIntegerPadding(String cell) {
+				return new Padding(" ".repeat(textWidth - cell.length()), "");
+			}
+
+			/**
+			 * @param cell
+			 * @return
+			 * @since 0.1.0
+			 */
+			private Padding getRealPadding(String cell) {
+				int index = cell.indexOf('.');
+				int left = index;
+				int right = cell.length() - index - 1;
+
+				return new Padding(//
+						" ".repeat(numberWidthLeft - left), //
+						"0".repeat(numberWidthRight - right));
+			}
+
+			/**
+			 * @param cell
+			 * @return
+			 * @since 0.1.0
+			 */
+			private Padding getTextPadding(String cell) {
+				return new Padding(//
+						"", //
+						" ".repeat(textWidth - cell.length()));
+			}
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		private ColumnContext[] columns;
+	}
+
+	/**
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	private record Padding(String before, String after) {
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	public static final TableStringGenerator DEFAULT = new TableStringGenerator();
+
+	/**
+	 * @since 0.1.0
+	 */
+	private TableStringGenerator() {
+		// nothing to do
+	}
+
+	/**
+	 * DOCME add JavaDoc for method toString
+	 * 
+	 * @param body
+	 * @return {@link String}
+	 * @since 0.1.0
+	 */
+	public String toString(String[][] body) {
+		return toString(body, null);
+	}
+
+	/**
+	 * DOCME add JavaDoc for method toString
+	 * 
+	 * @param body
+	 * @param headline
+	 * @return
+	 * @since 0.1.0
+	 */
+	public String toString(String[][] body, String[] headline) {
+		if (headline == null && body.length == 0) {
+			return "";
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		Context context = getContext(body, headline);
+
+		if (headline != null) {
+			appendRow(headline, sb, context);
+			appendHeadlineSeparator(headline, sb, context);
+		}
+
+		for (int iRow = 0; iRow < body.length; iRow++) {
+			String[] row = body[iRow];
+
+			appendRow(row, sb, context);
+		}
+
+		sb.deleteCharAt(sb.length() - 1);
+
+		return sb.toString();
+	}
+
+	/**
+	 * @param row
+	 * @param sb
+	 * @param iColumn
+	 * @param context
+	 * @since 0.1.0
+	 */
+	private void appendCell(String[] row, StringBuilder sb, int iColumn, Context context) {
+		Padding padding = getPadding(row, iColumn, context);
+
+		sb.append(padding.before + row[iColumn] + padding.after);
+	}
+
+	/**
+	 * @param headline
+	 * @param sb
+	 * @param context
+	 * @since 0.1.0
+	 */
+	private void appendHeadlineSeparator(String[] headline, StringBuilder sb, Context context) {
+		for (int iColumn = 0; iColumn < headline.length; iColumn++) {
+			sb.append("─".repeat(context.columns[iColumn].textWidth));
+
+			if (iColumn < headline.length - 1) {
+				sb.append("─┼─");
+			}
+		}
+
+		sb.append("\n");
+	}
+
+	/**
+	 * @param row
+	 * @param sb
+	 * @param context
+	 * @since 0.1.0
+	 */
+	private void appendRow(String[] row, StringBuilder sb, Context context) {
+		for (int iColumn = 0; iColumn < row.length; iColumn++) {
+			appendCell(row, sb, iColumn, context);
+
+			if (iColumn < row.length - 1) {
+				sb.append(" │ ");
+			}
+		}
+
+		sb.append("\n");
+	}
+
+	/**
+	 * @param body
+	 * @param headline
+	 * @param iColumn
+	 * @return
+	 * @since 0.1.0
+	 */
+	private List<String> getColumn(String[][] body, String[] headline, int iColumn) {
+		List<String> result = new ArrayList<>();
+
+		if (headline != null) {
+			result.add(headline[iColumn]);
+		}
+
+		for (int iRow = 0; iRow < body.length; iRow++) {
+			result.add(body[iRow][iColumn]);
+		}
+
+		return result;
+	}
+
+	/**
+	 * DOCME add JavaDoc for method getColumnContext
+	 * 
+	 * @param body
+	 * @param headline
+	 * @param iColumn
+	 * @return
+	 * @since 0.1.0
+	 */
+	private Context.ColumnContext getColumnContext(String[][] body, String[] headline, int iColumn) {
+		List<String> column = getColumn(body, headline, iColumn);
+
+		return new Context.ColumnContext(column);
+	}
+
+	/**
+	 * @param body
+	 * @param headline
+	 * @return
+	 * @since 0.1.0
+	 */
+	private int getColumnCount(String[][] body, String[] headline) {
+		if (headline != null) {
+			return headline.length;
+		}
+
+		return body[0].length;
+	}
+
+	/**
+	 * DOCME add JavaDoc for method getContext
+	 * 
+	 * @param body
+	 * @param headline
+	 * @return
+	 * @since 0.1.0
+	 */
+	private Context getContext(String[][] body, String[] headline) {
+		Context context = new Context();
+		context.columns = new Context.ColumnContext[getColumnCount(body, headline)];
+
+		for (int iColumn = 0; iColumn < context.columns.length; iColumn++) {
+			context.columns[iColumn] = getColumnContext(body, headline, iColumn);
+		}
+
+		return context;
+	}
+
+	/**
+	 * DOCME add JavaDoc for method getPadding
+	 * 
+	 * @param row
+	 * @param iColumn
+	 * @param context
+	 * @return
+	 * @since 0.1.0
+	 */
+	private Padding getPadding(String[] row, int iColumn, Context context) {
+		return context.columns[iColumn].getPadding(row[iColumn]);
+	}
+}
---
+++ src/test/java/de/voomdoon/util/tostring/table/TableStringGeneratorTest.java
@@ -0,0 +1,199 @@
+package de.voomdoon.util.tostring.table;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import de.voomdoon.testing.tests.TestBase;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+class TableStringGeneratorTest {
+
+	/**
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	@Nested
+	class String2_String_Test extends TestBase {
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test() throws Exception {
+			logTestStart();
+
+			String[][] table = { { "a", "b", "c" } };
+
+			String actual = toString(table, new String[] { "A", "B", "C" });
+
+			assertThat(actual).isEqualTo("A │ B │ C\n──┼───┼──\na │ b │ c");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_body_empty() throws Exception {
+			logTestStart();
+
+			String actual = toString(new String[0][0], new String[] { "h1", "h2" });
+
+			assertThat(actual).isEqualTo("h1 │ h2\n───┼───");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_padding() throws Exception {
+			logTestStart();
+
+			String[][] table = { { "aa", "b" } };
+
+			String actual = toString(table, new String[] { "A", "B" });
+
+			assertThat(actual).isEqualTo("A  │ B\n───┼──\naa │ b");
+		}
+
+		/**
+		 * @param body
+		 * @param headline
+		 * @return
+		 * @since 0.1.0
+		 */
+		private String toString(String[][] body, String[] headline) {
+			String actual = TableStringGenerator.DEFAULT.toString(body, headline);
+
+			logger.debug("actual:\n" + actual);
+
+			return actual;
+		}
+	}
+
+	/**
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	@Nested
+	class String2_Test extends TestBase {
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_empty() throws Exception {
+			logTestStart();
+
+			String actual = toString(new String[0][0]);
+
+			assertThat(actual).isBlank();
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_padding_integer_and_word() throws Exception {
+			logTestStart();
+
+			String[][] body = { { "1", "2" }, { "aa", "b" }, { "a", "b" } };
+
+			String actual = toString(body);
+
+			assertThat(actual).isEqualTo(" 1 │ 2\naa │ b\na  │ b");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_padding_number_integer() throws Exception {
+			logTestStart();
+
+			String[][] body = { { "1", "2" }, { "11", "2" } };
+
+			String actual = toString(body);
+
+			assertThat(actual).isEqualTo(" 1 │ 2\n11 │ 2");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_padding_number_real_leadingSpace() throws Exception {
+			logTestStart();
+
+			String[][] body = { { "1.2", "2" }, { "12.3", "2" } };
+
+			String actual = toString(body);
+
+			assertThat(actual).isEqualTo(" 1.2 │ 2\n12.3 │ 2");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_padding_number_real_trailingZero() throws Exception {
+			logTestStart();
+
+			String[][] body = { { "1.2", "2" }, { "1.23", "2" } };
+
+			String actual = toString(body);
+
+			assertThat(actual).isEqualTo("1.20 │ 2\n1.23 │ 2");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_padding_word() throws Exception {
+			logTestStart();
+
+			String[][] body = { { "A", "B" }, { "aa", "b" } };
+
+			String actual = toString(body);
+
+			assertThat(actual).isEqualTo("A  │ B\naa │ b");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_row1() throws Exception {
+			logTestStart();
+
+			String[][] body = { { "a", "b", "c" } };
+
+			String actual = toString(body);
+
+			assertThat(actual).isEqualTo("a │ b │ c");
+		}
+
+		/**
+		 * @param table
+		 * @return
+		 * @since 0.1.0
+		 */
+		private String toString(String[][] table) {
+			String actual = TableStringGenerator.DEFAULT.toString(table);
+
+			logger.debug("actual:\n" + actual);
+
+			return actual;
+		}
+	}
+}
